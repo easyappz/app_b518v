9a2032ff91e345b391749196fa3bc1388843642c
IbragimKhaniev <newinterny@gmail.com>
2025-11-28 03:10:53 +0300

    Add migration for password authentication support

---
+++ api/migrations/0003_member_password_auth.py
@@ -0,0 +1,47 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0002_update_rank_choices'),
+    ]
+
+    operations = [
+        # Remove old unique constraint on telegram_id
+        migrations.RemoveConstraint(
+            model_name='member',
+            name='unique_telegram_id',
+        ),
+        
+        # Make telegram_id nullable and not unique
+        migrations.AlterField(
+            model_name='member',
+            name='telegram_id',
+            field=models.BigIntegerField(null=True, blank=True, db_index=True),
+        ),
+        
+        # Make username unique and indexed
+        migrations.AlterField(
+            model_name='member',
+            name='username',
+            field=models.CharField(max_length=255, unique=True, db_index=True),
+        ),
+        
+        # Add password_hash field
+        migrations.AddField(
+            model_name='member',
+            name='password_hash',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        
+        # Add back unique constraint on telegram_id with condition
+        migrations.AddConstraint(
+            model_name='member',
+            constraint=models.UniqueConstraint(
+                fields=['telegram_id'],
+                condition=models.Q(telegram_id__isnull=False),
+                name='unique_telegram_id'
+            ),
+        ),
+    ]
